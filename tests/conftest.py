edba2957ea94aa0a0e2db54b2497bcf8dd2d1b1d
Artiom Chizh <artemciz993@gmail.com>
2026-02-16 01:21:59 +0300

    add attach

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,9 +4,21 @@ from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+from utils import attach
+
+
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser():
     options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": '128.0',
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
     options.add_argument("--window-size=1920,1080")
     driver = webdriver.Remote(
         command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
@@ -15,3 +27,9 @@ def setup_browser():
     browser.config.driver = driver
 
     yield driver
+    attach.add_screenshot(driver)
+    attach.add_page_source(driver)
+    attach.add_console_logs(driver)
+    attach.add_video(driver)
+
+    driver.quit()
